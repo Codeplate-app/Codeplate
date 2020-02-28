45dd4c7c6a8e7cdaa546fb678772aedbef88c4e7
MrAnyx <1ptitigre@gmail.com>
2020-02-28 23:10:59 +0100

    [add] ionicons and bugs color sidebar

---
+++ src/react/SideBar/Sections/Sections.js
@@ -0,0 +1,25 @@
+import React from "react"
+import style from "./sections.css"
+import SVGInject from "@iconfu/svg-inject"
+
+export default class Section extends React.Component{
+
+  componentDidMount(){
+     SVGInject(document.querySelector("img.iconSection"));
+  }
+   
+   render() {
+
+      let className = "section"
+      if(this.props.active){
+         className+=" active"
+      }
+      
+      return (
+         <div className={className}>
+            <img src={this.props.icon} alt="icon" className="iconSection"/>
+            <span className="sectionTitle">{this.props.text}</span>
+         </div>
+      );
+   }
+}
--- src/react/SideBar/SideBar.js
+++ src/react/SideBar/SideBar.js
@@ -1,15 +1,22 @@
 import React from "react"
 import style from "./sidebar.css"
 import Icon from "./Icon/Icon"
-import Section from "./Section/Section"
+import Sections from "./Sections/Sections"
+import listIcon from "../../assets/icons/fontAwesome/solid/list-ul.svg"
+import forkIcon from "../../assets/icons/fontAwesome/solid/plus-circle.svg"
+import settingsIcon from "../../assets/icons/fontAwesome/solid/cog.svg"
 
 export default class SideBar extends React.Component{
+
 	render() {
 		return(
 			<div id="sidebar">
 				<Icon/>
 				<div id="links">
-					<Section text="bonjour"/>
+					<Sections icon={forkIcon} text="New Project" active={true}/>
+					<Sections icon={listIcon} text="Boilerplates"/>
+					<Sections icon={settingsIcon} text="Settings"/>
+					
 				</div>
 			</div>
 		);
