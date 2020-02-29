b8bdd87ffc8566ba436088df4d3a6c4e50313b60
MrAnyx <1ptitigre@gmail.com>
2020-02-29 11:57:02 +0100

    [fix] bug sidebar colors active link

--- src/react/SideBar/Sections/Sections.js
+++ src/react/SideBar/Sections/Sections.js
@@ -4,21 +4,23 @@ import SVGInject from "@iconfu/svg-inject"
 
 export default class Section extends React.Component{
 
-  componentDidMount(){
-     SVGInject(document.querySelector("img.iconSection"));
-  }
-   
+   constructor(props){
+      super(props)
+
+      this.className = (this.props.active) ? "section active" : "section"
+      this.icon = (this.props.active) ? this.props.icon.orange : this.props.icon.black
+   }
+
+   componentWillMount(){
+      SVGInject(document.querySelector("img.iconSection"));
+   }
+
    render() {
 
-      let className = "section"
-      if(this.props.active){
-         className+=" active"
-      }
-     
 
       return (
-         <div className={className}>
-            <img src={this.props.icon} alt="icon" className="iconSection"/>
+         <div className={this.className}>
+            <img src={this.icon} alt="icon" className="iconSection"/>
             <span className="sectionTitle">{this.props.text}</span>
          </div>
       );
--- src/react/SideBar/SideBar.js
+++ src/react/SideBar/SideBar.js
@@ -2,20 +2,40 @@ import React from "react"
 import style from "./sidebar.css"
 import Icon from "./Icon/Icon"
 import Sections from "./Sections/Sections"
+
 import listIcon from "../../assets/icons/fontAwesome/solid/list-ul.svg"
 import forkIcon from "../../assets/icons/fontAwesome/solid/plus-circle.svg"
 import settingsIcon from "../../assets/icons/fontAwesome/solid/cog.svg"
+import listIconOrange from "../../assets/icons/fontAwesome/solid/list-ul-orange.svg"
+import forkIconOrange from "../../assets/icons/fontAwesome/solid/plus-circle-orange.svg"
+import settingsIconOrange from "../../assets/icons/fontAwesome/solid/cog-orange.svg"
 
 export default class SideBar extends React.Component{
+
+	constructor(props){
+		super(props)
+		this.list = {
+			black: listIcon,
+			orange: listIconOrange
+		}
+		this.newProject = {
+			black: forkIcon,
+			orange: forkIconOrange
+		}
+		this.settings = {
+			black: settingsIcon,
+			orange: settingsIconOrange
+		}
+	}
 
 	render() {
 		return(
 			<div id="sidebar">
 				<Icon/>
 				<div id="links">
-					<Sections icon={listIcon} text="Boilerplates" active={true}/>
-					<Sections icon={forkIcon} text="New Project" active={true}/>
-					<Sections icon={settingsIcon} text="Settings" active={true}/>
+					<Sections icon={this.list} text="Boilerplates"/>
+					<Sections icon={this.newProject} text="New Project"/>
+					<Sections icon={this.settings} text="Settings" active={true}/>
 				</div>
 			</div>
 		);
